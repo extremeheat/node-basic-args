18a3aa49efab910ed38d7f5dea22b6d4bf15fc5d
extremeheat <extreme@protonmail.ch>
2024-04-06 16:35:07 -0400

    Fix string/number parsing (#6)
    * Fix string/number parsing
    
    Fix strings being read as numbers erasing data. For example --v 1.20 becoming [-v, 1.2].
    
    * Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
 declare module "basic-cli" {
   type Option = {
-    name: string;
     type: Boolean | String | Number;
     alias?: string;
     description?: string;
@@ -15,7 +14,7 @@ declare module "basic-cli" {
     throwOnError?: boolean, // Throw an error instead of calling process.exit() with help screen (default: false)
     errorOnExtra?: boolean, // Throw an error if there are extra arguments (default: false)
     helpCommand?: string, // The -- command for opening the built-in help screen (default: help)
-    options: Option[],
+    options: Record<string, Option>, // Object containing the list of options program takes. See README for doc.
     // Pre-process the yargs parsed arguments before basic-args does any handling
     preprocess(args: object): void
     // Return true here if the result is ok, otherwise a string to raise to the user along with help screen
