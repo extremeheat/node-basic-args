b1c7c202d68d0a62f194bf3315340c201a546b20
extremeheat <extreme@protonmail.ch>
2025-04-28 04:00:54 -0400

    Add positional arguments support (#8)
    * Add positional arguments support
    
    * Update README.md
    
    * Update index.d.ts
    
    * Update README.md
    
    * Update README.md
    
    * Update index.js

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,33 @@
-declare module "basic-cli" {
+declare module "basic-args" {
   type Option = {
-    type: Boolean | String | Number;
+    type: BooleanConstructor | StringConstructor | NumberConstructor;
     alias?: string;
     description?: string;
     default?: any;
   }
-  type Result = Record<string, boolean | string | number>
-  // The second argument is the custom arg array if any, otherwise default to `process.argv`  
+
+  type Positional = {
+    name: string;
+    type: BooleanConstructor | StringConstructor | NumberConstructor;
+    description?: string;
+  }
+
+  type Result = Record<string, boolean | string | number | { [key: string]: any }>
+
+  // The second argument is the custom arg array if any, otherwise defaults to `process.argv`
   export default function(options: {
-    name: string,
-    version: string,
-    description: string,
-    throwOnError?: boolean, // Throw an error instead of calling process.exit() with help screen (default: false)
-    errorOnExtra?: boolean, // Throw an error if there are extra arguments (default: false)
-    helpCommand?: string, // The -- command for opening the built-in help screen (default: help)
-    options: Record<string, Option>, // Object containing the list of options program takes. See README for doc.
+    name: string;
+    version: string;
+    description: string;
+    throwOnError?: boolean; // Throw an error instead of calling process.exit() with help screen (default: false)
+    errorOnExtra?: boolean; // Throw an error if there are extra arguments (default: false)
+    helpCommand?: string; // The -- command for opening the built-in help screen (default: help)
+    options?: Record<string, Option>; // Object containing the list of options program takes
+    positionals?: Positional[]; // Array of required positional arguments
+    examples?: string[]; // Array of usage example strings for the help screen
     // Pre-process the yargs parsed arguments before basic-args does any handling
-    preprocess(args: object): void
-    // Return true here if the result is ok, otherwise a string to raise to the user along with help screen
-    validate?: (args: Result) => void | string
-  }, arguments?: string[]): Result
+    preprocess?(args: Record<string, any>): void;
+    // Return true if the result is valid, otherwise a string to raise to the user along with help screen
+    validate?(args: Result): true | string;
+  }, args?: string[]): Result
 }
